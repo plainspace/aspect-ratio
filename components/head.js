25703a3481265751fa58efd95a373e2538a69586
John Meguerian <johnmeguerian@gmail.com>
2018-12-03 11:51:17 -0500

    add OG title

--- components/head.js
+++ components/head.js
@@ -2,29 +2,30 @@ import React from 'react'
 import NextHead from 'next/head'
 import { string } from 'prop-types'
 
-const defaultDescription = 'A simple, useful aspect ratio calculator'
+const defaultTitle = 'Aspect'
+const defaultDescription = 'A simple aspect ratio calculator.'
 const defaultOGURL = ''
 const defaultOGImage = ''
 
 const Head = props => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>{props.title || 'Aspect Ratio'}</title>
+    <title>{props.title || defaultTitle}</title>
     <meta
       name="description"
       content={props.description || defaultDescription}
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/static/icon.png" type="image/png" />
-    <meta property="og:title" content={props.title || ''} />
+    <meta property="og:title" content={props.title || defaultTitle} />
     <meta
       property="og:description"
       content={props.description || defaultDescription}
     />
     <meta name="twitter:site" content={props.url || defaultOGURL} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
+    <meta name="twitter:card" content="summary" />
+    {/* <meta name="twitter:image" content={props.ogImage || defaultOGImage} /> */}
+    {/* <meta property="og:image" content={props.ogImage || defaultOGImage} /> */}
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
   </NextHead>
