e6da4923d2ebcb644a6c66becec212e5ced1529a
John Meguerian <johnmeguerian@gmail.com>
2018-12-03 11:52:40 -0500

    add icon as OG:Image

--- components/head.js
+++ components/head.js
@@ -5,7 +5,7 @@ import { string } from 'prop-types'
 const defaultTitle = 'Aspect'
 const defaultDescription = 'A simple aspect ratio calculator.'
 const defaultOGURL = ''
-const defaultOGImage = ''
+const defaultOGImage = '/static/icon.png'
 
 const Head = props => (
   <NextHead>
@@ -24,8 +24,8 @@ const Head = props => (
     />
     <meta name="twitter:site" content={props.url || defaultOGURL} />
     <meta name="twitter:card" content="summary" />
-    {/* <meta name="twitter:image" content={props.ogImage || defaultOGImage} /> */}
-    {/* <meta property="og:image" content={props.ogImage || defaultOGImage} /> */}
+    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
+    <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
   </NextHead>
