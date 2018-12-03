4918096650b478c4619b8744957b6377a2450cc7
John Meguerian <johnmeguerian@gmail.com>
2018-12-02 23:17:45 -0500

    add links to info bar duh

--- components/Info.jsx
+++ components/Info.jsx
@@ -1,8 +1,12 @@
 const Info = () => (
   <div className="info-bar grid">
     <span className="info-bar__item">aspect ratio</span>
-    <span className="info-bar__item">made by @jmegs</span>
-    <span className="info-bar__item">source on github</span>
+    <span className="info-bar__item">
+      made by <a href="https://twitter.com/jmegs">@jmegs</a>
+    </span>
+    <span className="info-bar__item">
+      source on <a href="https://github.com/jmegs/aspect-ratio">github</a>
+    </span>
   </div>
 )
 
