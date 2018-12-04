b4c6d2f6b05e7d1ff310fa74a9a975533fcac069
John Meguerian <johnmeguerian@gmail.com>
2018-12-04 08:18:47 -0500

    fix info bar on mobile

--- components/Info.jsx
+++ components/Info.jsx
@@ -1,8 +1,8 @@
 const Info = () => (
   <div className="info-bar grid">
-    <span className="info-bar__item">aspect ratio</span>
+    <span className="info-bar__item">aspect</span>
     <span className="info-bar__item">
-      made by <a href="https://twitter.com/jmegs">@jmegs</a>
+      made by <a href="https://johnmeguerian.com">@jmegs</a>
     </span>
     <span className="info-bar__item">
       source on <a href="https://github.com/jmegs/aspect-ratio">github</a>
